7d02058c71edf1a20b12325f64301fd37302bfff
cikzh <marlonp33@gmail.com>
2017-11-09 11:46:08 +0100

    implemented simple filter

--- src/app/GlobalFilter.ts
+++ src/app/GlobalFilter.ts
@@ -1,16 +1,17 @@
 import { Operator } from './Operator';
+import { Filter } from './Filter';
 
 export class GlobalFilter
 {
     public name: string;
-    public filter: string;
+    public filter: Filter;
     public operator: Operator;
     public selected: boolean;
 
     constructor(name: string, filter: string, operator: Operator)
     {
         this.name = name;
-        this.filter = filter;
+        this.filter = new Filter(filter);
         this.operator = operator;
     }
 }
