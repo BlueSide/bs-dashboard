a8f12f5f0e26a9fcb1c6907921a33f5890cb75f7
cikzh <marlonp33@gmail.com>
2017-11-29 20:05:51 +0100

    wip navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
+declare var UIkit: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent
 {
     public appTitle: string = 'Blue Side Dashboard';
     public liveUpdate: boolean = true;
+
+    constructor()
+    {
+        // components can be called from the imported UIkit reference
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
 import { GlobalFiltersComponent } from './global-filters.component';
 import { ChartComponent } from './chart.component';
+import { NavComponent } from './nav.component';
 
 @NgModule({
   declarations: [
       AppComponent,
+      NavComponent,
       GlobalFiltersComponent,
       ChartComponent
   ],
